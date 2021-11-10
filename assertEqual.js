09431e16f269d3a90042f94fe8866ca69d90f939
Tanim Amin <tanimul.amin@alumni.carleton.ca>
2021-11-10 20:43:30 +0000

    Lotide initial commit

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+
+    if(actual === expected) {
+        console.log("✅ Assertion Passed : ", actual + " === " + expected);
+    }
+    else {
+        console.log("🛑 Assertion Failed : ", actual + " !== " + expected);
+    }
+}
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 2);
