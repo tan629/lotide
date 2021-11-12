f11ffba9fd860997bdcb504f38517d1b1f13d0ab
Tanimul Amin <tanimul.amin@versaterm.com>
2021-11-12 16:41:58 -0500

    W1-lotide: Added assertEqual function call to test

--- eqObjects.js
+++ eqObjects.js
@@ -57,14 +57,14 @@ const eqObjects = function(object1, object2) {
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+console.log(assertEqual(eqObjects(ab, ba),true)); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+console.log(assertEqual(eqObjects(ab, abc),false)); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+console.log(assertEqual(eqObjects(cd, dc), true)); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+console.log(assertEqual(eqObjects(cd, cd2), false)); // => false
