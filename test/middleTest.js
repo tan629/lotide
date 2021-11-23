2e72a794cc002b6802cada0b5ef49f81c84cd8ed
Tanim Amin <tanimul.amin@alumni.carleton.ca>
2021-11-23 16:09:29 +0000

    W3-lotide: Implement mocha/chai in middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
-console.log(assertArraysEqual(middle([1,2]), []));
+describe("#middle", () => {
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
 
-console.log(assertArraysEqual(middle([1,2,3]), [2]));
+  it("returns [2] for [1,2,3]", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
 
-console.log(assertArraysEqual(middle([1,2,3,4]), [2,3]));
+  it("returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+});
