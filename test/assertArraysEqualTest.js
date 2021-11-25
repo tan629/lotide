150e18d5b9fb27c7dc92b512fc7265786162d0fe
Tanim Amin <tanimul.amin@alumni.carleton.ca>
2021-11-25 03:56:16 +0000

    W3-lotide: Corrected output message of assertArraysEqual test.

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,15 +2,15 @@ const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 
 describe("#assertArraysEqual", () => {
-  it("returns Assertion Passed - arrays are identical : [" + ["1", "2", "3"] + "] and [" + ["1", "2", "3"] + "]", () => {
+  it("returns arrays are identical : [" + ["1", "2", "3"] + "] and [" + ["1", "2", "3"] + "]", () => {
     assert.strictEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]),true);
   });
   
-  it("returns Assertion Failed - arrays are not identical : [" + ["1", "2", "3", "5"] + "] and [" + ["1", "2", "3","4"] + "]", () => {
+  it("returns arrays are not identical : [" + ["1", "2", "3", "5"] + "] and [" + ["1", "2", "3","4"] + "]", () => {
     assert.strictEqual(assertArraysEqual(["1", "2", "3","5"], ["1", "2", "3","4"]),false);
   });
   
-  it("returns Assertion Failed - arrays are not identical : [" + ["1", "2"] + "] and [" + ["1", "2", "3","4"] + "]", () => {
+  it("returns arrays are not identical : [" + ["1", "2"] + "] and [" + ["1", "2", "3","4"] + "]", () => {
     assert.strictEqual(assertArraysEqual(["1", "2"], ["1", "2", "3","4"]),false);
   });
   
