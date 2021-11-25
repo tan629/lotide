bee44287cfbfc5fb10fc4bc7e89ae08b5fa2b501
Tanim Amin <tanimul.amin@alumni.carleton.ca>
2021-11-25 03:44:39 +0000

    W3-lotide: Corrected assertion of equal objects so that objects are shown clearly in the mocha test output.

--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -1,31 +1,33 @@
 const assert = require('chai').assert;
 const assertObjectsEqual = require('../assertObjectsEqual');
+const inspect = require('util').inspect;
+
 
 describe("#assertObjectsEqual", () => {
   const ab = { a: "1", b: "2" };
   const ba = { b: "2", a: "1" };
 
-  it("returns Assertion Passed - Objects are identical : [" + ab + "] === [" + ba + "]", () => {
+  it("returns Assertion Passed - Objects are identical : [" + inspect(ab) + "] === [" + inspect(ba) + "]", () => {
     
     assert.strictEqual(assertObjectsEqual(ab, ba),true);
   });
   
   const abc = { a: "1", b: "2", c: "3" };
 
-  it("returns Assertion Passed - Objects are not identical : [" + ab + "] !== [" + abc + "]", () => {
+  it("returns Assertion Passed - Objects are not identical : [" + inspect(ab) + "] !== [" + inspect(abc) + "]", () => {
     assert.strictEqual(assertObjectsEqual(ab, abc),false);
   });
   
   const cd = { c: "1", d: ["2", 3] };
   const dc = { d: ["2", 3], c: "1" };
 
-  it("returns Assertion Passed - Objects are identical : [" + cd + "] === [" + dc + "]", () => {
+  it("returns Assertion Passed - Objects are identical : [" + inspect(cd) + "] === [" + inspect(dc) + "]", () => {
     assert.strictEqual(assertObjectsEqual(cd, dc),true);
   });
     
   const cd2 = { c: "1", d: ["2", 3, 4] };
 
-  it("returns Assertion Passed - Objects are not identical : [" + cd + "] !== [" + cd2 + "]", () => {
+  it("returns Assertion Passed - Objects are not identical : [" + inspect(cd) + "] !== [" + inspect(cd2) + "]", () => {
     assert.strictEqual(assertObjectsEqual(cd, cd2),false);
   });
 
